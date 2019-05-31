c40b42b097acca04e587c226933f9849d0e46cd6
Erick Zanardo <erickzanardoo@gmail.com>
2019-05-30 22:14:37 -0300

    First commit

---
+++ audioplayers/plugin.go
@@ -0,0 +1,67 @@
+package audioplayers
+
+import (
+        "fmt"
+	      flutter "github.com/go-flutter-desktop/go-flutter"
+        "github.com/go-flutter-desktop/go-flutter/plugin"
+)
+
+const channelName = "xyz.luan/audioplayers"
+
+type AudioplayersPlugin struct{}
+
+var _ flutter.Plugin = &AudioplayersPlugin{}
+
+func (p *AudioplayersPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
+        channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
+        channel.HandleFunc("play", handlePlay)
+        channel.HandleFunc("resume", handleResume)
+        channel.HandleFunc("pause", handlePause)
+        channel.HandleFunc("stop", handleStop)
+        channel.HandleFunc("release", handleRelease)
+        channel.HandleFunc("seek", handleSeek)
+        channel.HandleFunc("setVolume", handleSetVolume)
+        channel.HandleFunc("setUrl", handleSetUrl)
+        channel.HandleFunc("setReleaseMode", handleSetUrl)
+        return nil
+}
+
+func handlePlay(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Play")
+        return int32(1), nil
+}
+
+func handleResume(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Resume")
+        return int32(1), nil
+}
+
+func handlePause(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Pause")
+        return int32(1), nil
+}
+
+func handleStop(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Stop")
+        return int32(1), nil
+}
+
+func handleRelease(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Release")
+        return int32(1), nil
+}
+
+func handleSeek(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("Seek")
+        return int32(1), nil
+}
+
+func handleSetVolume(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("setVolume")
+        return int32(1), nil
+}
+
+func handleSetUrl(arguments interface{}) (reply interface{}, err error) {
+        fmt.Printf("setReleaseMode")
+        return int32(1), nil
+}
--- path_provider/go.mod
+++ path_provider/go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/go-flutter-desktop/go-flutter v0.17.0
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.8.1
-	github.com/stretchr/testify v1.3.0 // indirect
 )
