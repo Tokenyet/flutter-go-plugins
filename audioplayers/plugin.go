de75cc69451e58389fdf13f6e99ea1ca2e8e8b9f
Erick Zanardo <erickzanardoo@gmail.com>
2019-06-04 22:28:19 -0300

    Audioplayers

--- audioplayers/plugin.go
+++ audioplayers/plugin.go
@@ -1,9 +1,17 @@
 package audioplayers
+// https://github.com/faiface/beep/wiki/Hello,-Beep!
 
 import (
         "fmt"
+        //"time"
+        "os"
+        "log"
 	      flutter "github.com/go-flutter-desktop/go-flutter"
         "github.com/go-flutter-desktop/go-flutter/plugin"
+
+        "github.com/faiface/beep"
+        "github.com/faiface/beep/mp3"
+        //"github.com/faiface/beep/speaker"
 )
 
 const channelName = "xyz.luan/audioplayers"
@@ -11,6 +19,10 @@ const channelName = "xyz.luan/audioplayers"
 type AudioplayersPlugin struct{}
 
 var _ flutter.Plugin = &AudioplayersPlugin{}
+
+var url string
+var loadedStreamer beep.Streamer
+var loadedFormat beep.Format
 
 func (p *AudioplayersPlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
         channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
@@ -28,6 +40,17 @@ func (p *AudioplayersPlugin) InitPlugin(messenger plugin.BinaryMessenger) error 
 
 func handlePlay(arguments interface{}) (reply interface{}, err error) {
         fmt.Printf("Play")
+        fmt.Printf("Url %s", url)
+
+        //speaker.Init(loadedFormat.SampleRate, loadedFormat.SampleRate.N(time.Second/10))
+
+        //done := make(chan bool)
+        //speaker.Play(beep.Seq(loadedStreamer, beep.Callback(func() {
+        //        done <- true
+        //})))
+
+        //<-done
+
         return int32(1), nil
 }
 
@@ -62,14 +85,26 @@ func handleSetVolume(arguments interface{}) (reply interface{}, err error) {
 }
 
 func handleSetUrl(arguments interface{}) (reply interface{}, err error) {
-        url := []byte(arguments.(map[interface{}]interface{})["url"].(string))
-        isLocal := bool(arguments.(map[interface{}]interface{})["isLocal"].(bool))
+        url = string(arguments.(map[interface{}]interface{})["url"].(string))
+        fmt.Printf("setUrl %s", url)
+        //isLocal := bool(arguments.(map[interface{}]interface{})["isLocal"].(bool))
 
-        fmt.Printf("setUrl")
-        fmt.Printf("%s", url)
-        fmt.Printf("%b", isLocal)
+        file, err := os.Open(url)
+        if err != nil {
+                log.Fatal(err)
+        }
 
-        return int32(1), nil
+        streamer, format, err := mp3.Decode(file)
+
+        loadedStreamer = streamer
+        loadedFormat = format
+
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        //defer streamer.Close()
+        return int32(1), err
 }
 
 func handleSetReleaseMode(arguments interface{}) (reply interface{}, err error) {
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/go-flutter-desktop/plugins
+
+go 1.12
